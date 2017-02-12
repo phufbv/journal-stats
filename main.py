fa287cef559552b4bd27d57e0e2c4633f56c955e
Morgan Hollis <mdjh@star.ucl.ac.uk>
2017-02-12 18:14:33 +0000

    Added base urls

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+
+apj_base = "http://iopscience.iop.org/issue/0004-637X/"
+apjl_base = "http://iopscience.iop.org/issue/2041-8205/"
+
+latest_issue = "836/1" #same for both ApJ and ApJL - issues run in parallel
+
+
