8d24912705c55cf76d97fefb9729f6a91a08cfc7
Morgan Hollis <mdjh@star.ucl.ac.uk>
2017-02-12 18:57:18 +0000

    Added loop through articles

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
+from BeautifulSoup import BeautifulSoup
 
-apj_base = "http://iopscience.iop.org/issue/0004-637X/"
-apjl_base = "http://iopscience.iop.org/issue/2041-8205/"
+apj_base = "http://iopscience.iop.org/article/10.3847/1538-4357/"
 
-latest_issue = "836/1" #same for both ApJ and ApJL - issues run in parallel
+latest_issue = "836/1/" #same for both ApJ and ApJL - issues run in parallel
 
 
-start_url = apj_base + latest_issue
-r = requests.get(start_url)
+num_articles = 1
+
+for art in range(1,num_articles+1):
+	url = apj_base + latest_issue + str(art)
+	print url
+	r = requests.get(url)
+	soup = BeautifulSoup(r.content)
+	print soup
