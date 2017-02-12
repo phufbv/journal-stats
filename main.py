975ac30733e856a512e6cfaab64dafd38a29440a
Morgan Hollis <mdjh@star.ucl.ac.uk>
2017-02-12 19:03:53 +0000

    Get dates from articles

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ apj_base = "http://iopscience.iop.org/article/10.3847/1538-4357/"
 latest_issue = "836/1/" #same for both ApJ and ApJL - issues run in parallel
 
 
-num_articles = 1
+num_articles = 1 #test using only first article for now
 
 for art in range(1,num_articles+1):
 	url = apj_base + latest_issue + str(art)
-	print url
+	
 	r = requests.get(url)
 	soup = BeautifulSoup(r.content)
-	print soup
+	
+	dates = soup.find('div', attrs={'class':'col-no-break wd-jnl-art-dates'})
+	#dates = soup.find('div', attrs={'class':'article-meta'})
+
+	print dates
