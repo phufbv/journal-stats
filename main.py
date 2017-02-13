c045bd0c3796be3dcd0655e023bb53a879840c2b
phufbv <m.hollis.26@gmail.com>
2017-02-13 12:55:41 +0000

    Parse and store final dates

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ latest_issue = "836/1/" #same for both ApJ and ApJL - issues run in parallel
 
 num_articles = 1 #test using only first article for now
 
-for art in range(1,num_articles+1):
-	url = apj_base + latest_issue + str(art)
+for num in range(1,num_articles+1):
+	url = apj_base + latest_issue + str(num)
 	
 	r = requests.get(url)
 	soup = BeautifulSoup(r.content)
@@ -17,4 +17,15 @@ for art in range(1,num_articles+1):
 	dates = soup.find('div', attrs={'class':'col-no-break wd-jnl-art-dates'})
 	#dates = soup.find('div', attrs={'class':'article-meta'})
 
-	print dates
+	
+	start = dates.find("Received ") + 9
+    	end = dates.find("Accepted")
+    
+    	received_date = dates[start:end]
+    
+    	start = dates.find("Accepted ") + 9
+    	end = dates.find("Published")
+    
+    	accepted_date = dates[start:end]
+    
+    	print received_date, accepted_date
