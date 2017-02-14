8421f73a851ee62ff886d27d7ba4bf7db1459497
Morgan Hollis <mdjh@star.ucl.ac.uk>
2017-02-14 00:28:12 +0000

    Adding loop through previous volumes/issues

--- main.py
+++ main.py
@@ -3,32 +3,39 @@ from BeautifulSoup import BeautifulSoup
 
 apj_base = "http://iopscience.iop.org/article/10.3847/1538-4357/"
 
-latest_issue = "836/1/"  # same for both ApJ and ApJL - issues run in parallel
+latest_volume = 836  # same for both ApJ and ApJL - issues run in parallel
+
+volume_list = range(latest_volume-11,latest_volume+1)  # latest 12 volumes, roughly 6 months
 
 
-num_articles = 5 #test using a few articles
+num_articles = 1  # test using a few articles
 
-for num in range(1,num_articles+1):
-    url = apj_base + latest_issue + str(num)
-    
-    try:
-        r = requests.get(url)
-        soup = BeautifulSoup(r.content)
-        
-        dates = soup.find('div', attrs={'class':'col-no-break wd-jnl-art-dates'}).text
-        #dates = soup.find('div', attrs={'class':'article-meta'})
+for volume in reversed(volume_list):
 
-        start = dates.find("Received ") + 9
-        end = dates.find("Accepted")
-    
-        received_date = dates[start:end]
-    
-        start = dates.find("Accepted ") + 9
-        end = dates.find("Published")
-    
-        accepted_date = dates[start:end]
-    
-        print received_date, accepted_date
-    
-    except:
-        print "Some error occurred (URL '",url,"' not available?). Skipping."
+	vol_issue = str(volume) + "/1/"
+
+	for num in range(1,num_articles+1):
+
+	    url = apj_base + vol_issue + str(num)
+	    
+	    try:
+	    	r = requests.get(url)
+	        soup = BeautifulSoup(r.content)
+	        
+	        dates = soup.find('div', attrs={'class':'col-no-break wd-jnl-art-dates'}).text
+	        #dates = soup.find('div', attrs={'class':'article-meta'})
+
+	        start = dates.find("Received ") + 9
+	        end = dates.find("Accepted")
+	    
+	        received_date = dates[start:end]
+	    
+	        start = dates.find("Accepted ") + 9
+	        end = dates.find("Published")
+	    
+	        accepted_date = dates[start:end]
+	    
+	        print received_date, accepted_date
+	    
+	    except:
+	        print "Some error occurred (URL '",url,"' not available?). Skipping."
