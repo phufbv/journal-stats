5900fcfe5efdf6ff8c41da4bd833c26a8c0471d1
Morgan Hollis <mdjh@star.ucl.ac.uk>
2017-02-14 01:26:58 +0000

    Bugfix - ApJ URLs change format at volume 833

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from BeautifulSoup import BeautifulSoup
 
 apj_base = "http://iopscience.iop.org/article/10.3847/1538-4357/"
+apj_base2 = "http://iopscience.iop.org/article/10.3847/0004-637X/"  # different base URL before vol.833 iss.2 for some reason
 
 latest_volume = 836  # same for both ApJ and ApJL - issues run in parallel
 
@@ -16,26 +17,29 @@ for volume in reversed(volume_list):
 
 	for num in range(1,num_articles+1):
 
-	    url = apj_base + vol_issue + str(num)
+		if volume > 833:
+			url = apj_base + vol_issue + str(num)
+		else:
+			url = apj_base2 + vol_issue + str(num)
 	    
-	    try:
-	    	r = requests.get(url)
-	        soup = BeautifulSoup(r.content)
+		try:
+			r = requests.get(url)
+			soup = BeautifulSoup(r.content)
 	        
-	        dates = soup.find('div', attrs={'class':'col-no-break wd-jnl-art-dates'}).text
-	        #dates = soup.find('div', attrs={'class':'article-meta'})
+			dates = soup.find('div', attrs={'class':'col-no-break wd-jnl-art-dates'}).text
+			#dates = soup.find('div', attrs={'class':'article-meta'})
 
-	        start = dates.find("Received ") + 9
-	        end = dates.find("Accepted")
+			start = dates.find("Received ") + 9
+			end = dates.find("Accepted")
 	    
-	        received_date = dates[start:end]
+			received_date = dates[start:end]
 	    
-	        start = dates.find("Accepted ") + 9
-	        end = dates.find("Published")
+			start = dates.find("Accepted ") + 9
+			end = dates.find("Published")
 	    
-	        accepted_date = dates[start:end]
+			accepted_date = dates[start:end]
 	    
-	        print received_date, accepted_date
+			print received_date, accepted_date
 	    
-	    except:
-	        print "Some error occurred (URL '",url,"' not available?). Skipping."
+		except:
+			print "Some error occurred (URL '",url,"' not available?). Skipping."
