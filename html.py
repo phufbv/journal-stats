627ef7b496467d8a8d498f6c74e63e8d7dbbfb50
Morgan Hollis <mdjh@star.ucl.ac.uk>
2017-02-18 22:26:30 +0000

    Added ApJL URLs

--- html.py
+++ html.py
@@ -1,20 +1,32 @@
 import requests
 from BeautifulSoup import BeautifulSoup
 
-apj_base = "http://iopscience.iop.org/article/10.3847/1538-4357/"
-apj_base2 = "http://iopscience.iop.org/article/10.3847/0004-637X/"  # different base URL before vol.833 iss.2 for some reason
-
-apjl_base = "http://iopscience.iop.org/article/10.3847/2041-8213/"
-
 
 def build_url(volume, issue, number):
 	
+	apj_base = "http://iopscience.iop.org/article/10.3847/1538-4357/"
+	apj_base2 = "http://iopscience.iop.org/article/10.3847/0004-637X/"  # different base URL before vol.833 iss.2 for some reason
+
 	vol_issue = str(volume) + "/" + str(issue) + "/"
 
 	if volume > 833:
 	    url = apj_base + vol_issue + str(number)
 	else:
 	    url = apj_base2 + vol_issue + str(number)
+
+	return url
+
+def build_letters_url(volume, issue, number):
+	
+	apj_base = "http://iopscience.iop.org/article/10.3847/2041-8213/"
+	apj_base2 = "http://iopscience.iop.org/article/10.3847/2041-8205/"  # different base URL before vol.833 iss.2 for some reason
+
+	vol_issue = str(volume) + "/" + str(issue) + "/"
+
+	if volume > 833:
+	    url = apj_base + vol_issue + 'L' + str(number)
+	else:
+	    url = apj_base2 + vol_issue + 'L' + str(number)
 
 	return url
 
