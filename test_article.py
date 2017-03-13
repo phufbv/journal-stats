e93875200185a9cad64e7a4754e2e4cc43d1eb54
Morgan Hollis <mdjh@star.ucl.ac.uk>
2017-03-13 01:07:10 +0000

    Updated ArticleTests

--- test_article.py
+++ test_article.py
@@ -1,11 +1,14 @@
 from unittest import TestCase
 from article import Article
 
+
 class ArticleTests(TestCase):
+
 
     def setUp(self):
     	test_string = 'DatesReceived 2016 November 21Accepted 2017 January 8Published2017 February 3'
         self.article = Article(test_string)
+
 
     def test_article_stores_dates(self):
         expectedReceivedDate = '2016 November 21'
@@ -16,12 +19,18 @@ class ArticleTests(TestCase):
         self.assertEqual(expectedReceivedDate, dates[0])
         self.assertEqual(expectedAcceptedDate, dates[1])
 
+
     def test_article_calculates_date_difference(self):
         expectedDaysDifference = 48
 
         daysDifference = self.article.get_dates()[2]
 
     	self.assertEqual(expectedDaysDifference, daysDifference)
+
+    def test_article_stores_acceptance_year(self):
+        expectedYear = int('2017')
+
+        self.assertEqual(expectedYear, self.article.year)
 
 
 if __name__ == '__main__':
