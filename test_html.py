fb1aab08ee5b8dcf5403733fa5d2765b42d13743
Morgan Hollis <mdjh@star.ucl.ac.uk>
2017-04-20 19:59:33 +0100

    Added HtmlTests (extracting date string)

--- test_html.py
+++ test_html.py
@@ -14,49 +14,47 @@ class HtmlTests(TestCase):
 
 
     def test_html_builds_apj_url(self):
-        #if
+        journal = "ApJ"
         expectedUrl = self.base_url + "1538-4357/835/1/3"
 
-        #when
-        url = html.build_url("ApJ", self.test_vol, self.test_iss, self.test_num)
+        url = html.build_url(journal, self.test_vol, self.test_iss, self.test_num)
 
-        #then
         self.assertEqual(expectedUrl, url)
 
 
     def test_html_builds_apjl_url(self):
-        #if
+        journal = "ApJL"
         expectedUrl = self.base_url + "2041-8213/835/1/L3"
 
-        #when
-        url = html.build_url("ApJL", self.test_vol, self.test_iss, self.test_num)
+        url = html.build_url(journal, self.test_vol, self.test_iss, self.test_num)
 
-        #then
         self.assertEqual(expectedUrl, url)
 
 
     def test_html_builds_correct_url_before_vol_833(self):
-        #if
         test_vol = 831
         expectedUrl = self.base_url + "2041-8205/831/1/L3"
 
-        #when
         url = html.build_url("ApJL", test_vol, self.test_iss, self.test_num)
 
-        #then
         self.assertEqual(expectedUrl, url)
 
 
     def test_html_builds_correct_url_for_vol_833(self):
-        #if
         test_vol = 833
         expectedUrl = self.base_url + "2041-8205/833/1/L3"
 
-        #when
         url = html.build_url("ApJL", test_vol, self.test_iss, self.test_num)
 
-        #then
         self.assertEqual(expectedUrl, url)
+
+
+    def test_html_extracts_correct_div_from_html_page(self):
+        expectedDateString = 'DatesReceived2016 November 21Accepted2017 January 8Published2017 February 3'
+
+        dateString = html.get_date_div("http://iopscience.iop.org/article/10.3847/1538-4357/836/1/1")
+
+        self.assertEqual(expectedDateString, dateString)
 
 
 if __name__ == '__main__':
