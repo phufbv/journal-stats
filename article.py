005b6104fb4dac4655ecbbabd0f604b55f222413
Morgan Hollis <mdjh@star.ucl.ac.uk>
2017-04-20 20:32:47 +0100

    Bugfix - handling different date string formats

--- article.py
+++ article.py
@@ -4,8 +4,8 @@ from datetime import datetime
 class Article:
 
 	def __init__(self, date_string):
-		self.date_string = date_string
-		
+		self.date_string = date_string.replace(" ", "")
+
 
 	def get_dates(self):
 		received_date = self.__find_received_date()
@@ -25,21 +25,21 @@ class Article:
 
 
 	def __find_received_date(self):
-		start = self.date_string.find("Received ") + 9
+		start = self.date_string.find("Received") + 8
 		end = self.date_string.find("Accepted")
 		    
 		return self.date_string[start:end]
 
 
 	def __find_accepted_date(self):
-		start = self.date_string.find("Accepted ") + 9
+		start = self.date_string.find("Accepted") + 8
 		end = self.date_string.find("Published")
 		    
 		return self.date_string[start:end]
 
 
 	def __find_difference(self, date1, date2):
-		d1 = datetime.strptime(date1, "%Y %B %d")
-		d2 = datetime.strptime(date2, "%Y %B %d")
+		d1 = datetime.strptime(date1, "%Y%B%d")
+		d2 = datetime.strptime(date2, "%Y%B%d")
 		    
 		return abs((d2 - d1).days)
--- test_article.py
+++ test_article.py
@@ -11,8 +11,8 @@ class ArticleTests(TestCase):
 
 
     def test_article_stores_dates(self):
-        expectedReceivedDate = '2016 November 21'
-    	expectedAcceptedDate = '2017 January 8'
+        expectedReceivedDate = '2016November21'
+    	expectedAcceptedDate = '2017January8'
 
         dates = self.article.get_dates()
 
@@ -32,6 +32,15 @@ class ArticleTests(TestCase):
         expectedYear = 2017
 
         self.assertEqual(expectedYear, self.article.get_year())
+
+
+    def test_article_handles_different_date_string_format(self):
+        test_string = 'DatesReceived2016 November 21Accepted2017 January 8Published2017 February 3'
+        article = Article(test_string)
+
+        expectedYear = 2017
+
+        self.assertEqual(expectedYear, article.get_year())
 
 
 if __name__ == '__main__':
