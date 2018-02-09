b4a6432b86725fc8c1f34cb98f267175dff1252c
Morgan Hollis <mdjh@star.ucl.ac.uk>
2018-02-09 00:32:52 +0000

    Refactoring - removed build_url() function and redundant url-building tests

--- html.py
+++ html.py
@@ -21,32 +21,6 @@ def detect_start_volume():
 	return (start_volume, current_year-1)
 
 
-def build_url(journal, volume, issue, number):
-	
-	base_url = "http://iopscience.iop.org/article/10.3847/" 
-
-	if journal == "APJ":
-		vol_issue_num = str(volume) + "/" + str(issue) + "/" + str(number)
-		
-		pre_vol_833 = "0004-637X/"
-		post_vol_833 = "1538-4357/"
-	
-	elif journal == "APJL":
-		vol_issue_num = str(volume) + "/" + str(issue) + "/" + 'L' + str(number)
-		
-		pre_vol_833 = "2041-8205/"
-		post_vol_833 = "2041-8213/"
-
-
-	# URLs slightly different before vol.833 iss.2 for some reason
-	if volume > 833:
-	    url = base_url + post_vol_833 + vol_issue_num
-	else:
-	    url = base_url + pre_vol_833 + vol_issue_num
-
-	return url
-
-
 def build_urls(journal, volume, issue):
 
 	base_url = "http://iopscience.iop.org"
--- test_html.py
+++ test_html.py
@@ -6,11 +6,8 @@ class HtmlTests(TestCase):
 
 
 	def setUp(self):
-		self.base_url = "http://iopscience.iop.org/article/10.3847/"
-
-		self.test_vol = 835
-		self.test_iss = 1
-		self.test_num = 3
+		self.test_vol = 836
+		self.test_iss = 2
 
 
 	def test_can_detect_start_volume_in_current_year(self):
@@ -23,46 +20,6 @@ class HtmlTests(TestCase):
 		self.assertEqual(expectedStartVol, start_volume)
 
 
-	def test_html_builds_apj_url(self):
-		#Functionally replaced by test_html_extracts_article_urls_from_contents_page
-		journal = "APJ"
-		expectedUrl = self.base_url + "1538-4357/835/1/3"
-
-		url = html.build_url(journal, self.test_vol, self.test_iss, self.test_num)
-
-		self.assertEqual(expectedUrl, url)
-
-
-	def test_html_builds_apjl_url(self):
-		#Functionally replaced by test_html_extracts_article_urls_from_contents_page
-		journal = "APJL"
-		expectedUrl = self.base_url + "2041-8213/835/1/L3"
-
-		url = html.build_url(journal, self.test_vol, self.test_iss, self.test_num)
-
-		self.assertEqual(expectedUrl, url)
-
-
-	def test_html_builds_correct_url_before_vol_833(self):
-		#Functionally replaced by test_html_extracts_article_urls_from_contents_page
-		test_vol = 831
-		expectedUrl = self.base_url + "2041-8205/831/1/L3"
-
-		url = html.build_url("APJL", test_vol, self.test_iss, self.test_num)
-
-		self.assertEqual(expectedUrl, url)
-
-
-	def test_html_builds_correct_url_for_vol_833(self):
-		#Functionally replaced by test_html_extracts_article_urls_from_contents_page
-		test_vol = 833
-		expectedUrl = self.base_url + "2041-8205/833/1/L3"
-
-		url = html.build_url("APJL", test_vol, self.test_iss, self.test_num)
-
-		self.assertEqual(expectedUrl, url)
-
-
 	def test_html_extracts_correct_div_from_html_page(self):
 		expectedDateString = 'DatesReceived2016 November 21Accepted2017 January 8Published2017 February 3'
 
@@ -73,8 +30,8 @@ class HtmlTests(TestCase):
 
 	def test_html_extracts_article_urls_from_contents_page_apj(self):
 		journal = "APJ"
-		volume = 836
-		issue = 2
+		volume = self.test_vol
+		issue = self.test_iss
 
 		expectedArticleUrls = ["http://iopscience.iop.org/article/10.3847/1538-4357/aa5be8","http://iopscience.iop.org/article/10.3847/1538-4357/aa5b8b","http://iopscience.iop.org/article/10.3847/1538-4357/aa5b88","http://iopscience.iop.org/article/10.3847/1538-4357/836/2/152","http://iopscience.iop.org/article/10.3847/1538-4357/836/2/153"]
 		#URLs of first 5 articles in this issue
@@ -87,8 +44,8 @@ class HtmlTests(TestCase):
 
 	def test_html_extracts_article_urls_from_contents_page_apjl(self):
 		journal = "APJL"
-		volume = 836
-		issue = 2
+		volume = self.test_vol
+		issue = self.test_iss
 
 		expectedArticleUrls = ["http://iopscience.iop.org/article/10.3847/2041-8213/836/2/L17","http://iopscience.iop.org/article/10.3847/2041-8213/aa5dab","http://iopscience.iop.org/article/10.3847/2041-8213/aa5cb0","http://iopscience.iop.org/article/10.3847/2041-8213/aa5eb0","http://iopscience.iop.org/article/10.3847/2041-8213/aa5dee"]
 		#URLs of first 5 articles in this issue
