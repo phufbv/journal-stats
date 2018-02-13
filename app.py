a7649af5bfa98c996a4aed91a44b9a0aa82dee4c
Morgan Hollis <mdjh@star.ucl.ac.uk>
2018-02-13 00:04:10 +0000

    Refactoring

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
-import sys
 from flask import Flask, render_template, request
 
 import parameters as pars
-import html
-from article import Article
-from file_writer import FileWriter
+import script
 
 
 app = Flask(__name__)
@@ -22,50 +19,11 @@ def form_post():
 	num_articles = request.form['num_articles']
 
 	if pars.valid(journal, num_articles):
-		run_script(journal, num_articles)
+		script.run(journal.upper(), num_articles)
 	else:
 		return render_template('input_error.html')
 
 	return "Complete!"
-
-
-def run_script(journal, num_articles):
-	# Setup output file, and use brief run if testing
-	writer = FileWriter(pars.filename)
-	issue = 1  # sample issue for each volume
-	if len(sys.argv) > 1:
-		print "Testing....."
-		num_volumes = 1
-	else:
-		num_volumes = 18  # 18 volumes per year
-
-
-	# Sample papers accepted in previous year
-	date = html.detect_start_volume()
-	start_volume = date[0]
-	acceptance_year = date[1]
-
-	volumes = range(start_volume-num_volumes+1, start_volume+1)
-
-
-	# for volume in reversed(volumes):
-
-	# 	for number in range(1, pars.num_articles+1):
-
-	# 		url = html.build_url(pars.journal, volume, issue, number)
-		    
-	# 		try:
-	# 			date_string = html.get_date_div(url)
-	# 		except:
-	# 			print "Some error occurred (URL '",url,"' not available?). Skipping."
-	# 			break
-
-	# 		article = Article(date_string)
-
-	# 		if article.get_year() == acceptance_year:
-	# 			writer.write_to_file(article)
-
-	writer.close_file()
 
 
 if __name__ == "__main__":
